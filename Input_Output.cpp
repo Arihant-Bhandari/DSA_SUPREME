fe7da2d3b99304f1b7ffebfd10db6dd9c222cebe
Arihant Bhandari <arihant.bhandari.2004@gmail.com>
2024-09-17 09:23:50 +0530

    Program: Input Output

---
+++ Input_Output.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void inOut()
+{
+    int a;
+    cout<<"Enter a Number: ";
+    cin>>a;
+    cout<<"Entered: "<<a;
+}
+int main()
+{
+    inOut();
+    return 0;
+}
