355796ecee689df66d183e00ba6f95079493a6b3
Arihant Bhandari <arihant.bhandari.2004@gmail.com>
2024-11-19 03:06:06 +0530

    Program: Inverted Full Pyramid Pattern

--- Patterns/Full_Pyramid_pattern.cpp
+++ Patterns/Full_Pyramid_pattern.cpp
@@ -12,7 +12,7 @@ int main()
         for(int j = n-i-1; j>0; j--)
         cout<<"  ";
 
-        for(int j = 0;j<2*i+1;j++)
+        for(int j = 0; j<2*i+1; j++)
         cout<<"* ";
 
         cout<<endl;
--- Patterns/Inverted_Full_Pyramid_pattern.cpp
+++ Patterns/Inverted_Full_Pyramid_pattern.cpp
@@ -9,16 +9,11 @@ int main()
 
     for(int i = 0; i < n; i++)
     {
-        for(int j = n-i-1; j>0; j--)
+        for(int j = 0; j<i; j++)
         cout<<"  ";
 
-        for(int j = 0; j<(2*i+1); j++)
-        {
-            if(j==0 || j==(2*i) || i==(n-1))
-            cout<<"* ";
-            else
-            cout<<"  ";
-        }
+        for(int j = 2*(n-i)-1; j>0; j--)
+        cout<<"* ";
 
         cout<<endl;
     }
