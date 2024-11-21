0005c0b74989c295b646b1c1e1d02ae80d258abb
Arihant Bhandari <arihant.bhandari.2004@gmail.com>
2024-11-21 22:57:05 +0530

    Program: Number Diamond Pattern

---
+++ Patterns/Number_Diamond_pattern.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter n: ";
+    cin>>n;
+
+    int init;
+
+    for(int i = 1; i<=n; i++)
+    {
+        for(int j = 0; j<n-i; j++)
+        cout<<"  ";
+
+        init = 1;
+        for(int j = i; j>0; j--)
+        {
+            cout << init++;
+            cout << " ";
+        }
+
+        --init;
+        for(int j = 1; j<i; j++)
+        {
+            cout << --init;
+            cout << " ";
+        }
+
+        cout<<endl;
+    }
+    for(int i = n-1; i>0; i--)
+    {
+        for(int j = 0; j<n-i; j++)
+        cout<<"  ";
+
+        init = 1;
+        for(int j = i; j>0; j--)
+        {
+            cout << init++;
+            cout << " ";
+        }
+
+        --init;
+        for(int j = 1; j<i; j++)
+        {
+            cout << --init;
+            cout << " ";
+        }
+
+        cout<<endl;
+    }
+    return 0;
+}
