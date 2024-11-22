d02b86efa6cdf41b76ffe71cf87fe9f356461d2c
Arihant Bhandari <arihant.bhandari.2004@gmail.com>
2024-11-22 18:39:51 +0530

    Program: Numeric Hollow Half Pyramid Pattern

---
+++ Patterns/Numeric_hollow_Half_Pyramid_pattern.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n; 
+    cout<<"Enter n: ";
+    cin>>n;
+
+    for(int i = 0; i<n; i++)
+    {
+        for(int j = 0; j<=i; j++)
+        {
+            if(i==n-1 || j==0 || j==i)
+            cout<<j+1;
+            else
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
