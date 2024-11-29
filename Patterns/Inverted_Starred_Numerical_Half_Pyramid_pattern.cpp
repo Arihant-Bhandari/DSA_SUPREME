d0ac39d14114437de47ae7f7a0d86d5ba4c800ed
Arihant Bhandari <arihant.bhandari.2004@gmail.com>
2024-11-29 18:28:24 +0530

    Program: Inverted Starred Numerical Half Pyramid Pattern

---
+++ Patterns/Inverted_Starred_Numerical_Half_Pyramid_pattern.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter n: ";
+    cin>>n;
+
+    for(int i = n; i>0; i--)
+    {
+        for(int j = 1; j<=2*i-1; j++)
+        {
+            if(j%2==1)
+            cout<<i;
+            else
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
