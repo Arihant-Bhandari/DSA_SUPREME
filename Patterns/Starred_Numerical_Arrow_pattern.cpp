fbcbfc19fc0020cbf453fe50f5e6a406f871069f
Arihant Bhandari <arihant.bhandari.2004@gmail.com>
2024-11-29 18:28:38 +0530

    Program: Starred Numerical Arrow Pattern

---
+++ Patterns/Starred_Numerical_Arrow_pattern.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter n: ";
+    cin>>n;
+
+    for(int i = 0; i<n; i++)
+    {
+        for(int j = 1; j<=2*i+1; j++)
+        {
+            if(j%2==1)
+            cout<<i+1;
+            else
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for(int i = n-1; i>0; i--)
+    {
+        for(int j = 1; j<=2*i-1; j++)
+        {
+            if(j%2==1)
+            cout<<i;
+            else
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
