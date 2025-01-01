34700423d4c0b1c06f36e3b64e8a9b9c83878d89
Arihant Bhandari <arihant.bhandari.2004@gmail.com>
2025-01-01 23:26:54 +0530

    Program: Check if Prime

---
+++ Functions/Prime_number.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+bool primeNum(int n)
+{
+    if(n<=1)
+    return false;
+    
+    for(int i = 2; i <= sqrt(n); i++)
+    {
+        if(n % i == 0)
+        return false;
+    }
+    return true;
+}
+int main()
+{
+    int n;
+    cout<<"Enter a no.: ";
+    cin>> n;
+
+    if(primeNum(n))
+    cout << n << " is a Prime Number.";
+    else
+    cout << n << " is not a Prime Number.";
+    return 0;
+}
