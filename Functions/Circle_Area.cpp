8159381fdf4e43ec66b5acb562404b07d5d70b46
Arihant Bhandari <arihant.bhandari.2004@gmail.com>
2025-01-02 00:22:57 +0530

    Program: Area of Circle

---
+++ Functions/Circle_Area.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+float area(float radius)
+{
+    return M_PI*M_PI*radius;
+}
+int main()
+{
+    float radius;
+    cout<<"Enter the Radius of given Circle: ";
+    cin>> radius;
+
+    cout<< area(radius) << " is the area of Cicle with given Radius.";
+    return 0;
+}
