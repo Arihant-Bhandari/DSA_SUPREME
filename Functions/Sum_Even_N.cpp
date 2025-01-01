6b30045bffeee94b1ecb4ac1d31253382cc9fa27
Arihant Bhandari <arihant.bhandari.2004@gmail.com>
2025-01-01 23:30:36 +0530

    Program: Even Summation till N

---
+++ Functions/Sum_Even_N.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int summation(int n)
+{
+    int sum = 0;
+
+    for(int i = 0; i <= n; i+=2)
+    sum += i;
+
+    return sum;
+}
+int main()
+{
+    int n;
+    cout<<"Enter a no.: ";
+    cin>> n;
+
+    cout << "The even summation till " << n << " is: " << summation(n);
+    return 0;
+}
