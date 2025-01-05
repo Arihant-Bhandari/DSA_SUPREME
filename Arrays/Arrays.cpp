94394253892c12f8e194dabf4fc1a3b51e8f4815
Arihant Bhandari <arihant.bhandari.2004@gmail.com>
2025-01-05 22:01:15 +0530

    Program: Introduction to ARRAYS

---
+++ Arrays/Arrays.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // // creating basic arrays
+    // short s[5];
+    // int i[10];
+    // long l[20];
+    // float f[10];
+    // double d[15];
+    // char c[3];
+    // bool b[2];
+
+    // cout << "Base Address(int i[]): " << &i << endl;
+    // cout << "Base Address(short s[]): " << &s << endl;
+    // cout << "Base Address(long l[]): " << &l << endl;
+    // cout << "Base Address(float f[]): " << &f << endl;
+    // cout << "Base Address(double d[]): " << &d << endl;
+    // cout << "Base Address(char c[]): " << &c << endl;
+    // cout << "Base Address(bool b[]): " << &b << endl;
+    // cout << endl;
+    // cout << "Size of(int i[]): " << sizeof(i) << endl;
+    // cout << "Size of(short s[]): " << sizeof(s) << endl;
+    // cout << "Size of(long l[]): " << sizeof(l) << endl;
+    // cout << "Size of(float f[]): " << sizeof(f) << endl;
+    // cout << "Size of(double d[]): " << sizeof(d) << endl;
+    // cout << "Size of(char c[]): " << sizeof(c) << endl;
+    // cout << "Size of(bool b[]): " << sizeof(b) << endl;
+    
+    // initializing arrays
+    // int a[] = {1, 2, 3, 4, 5};
+    // int b[5] = {1, 2, 3, 4, 5};
+    // int c[5] = {1, 2, 3};
+    // int d[2] = {1, 2, 3};
+
+    // indexing of an array
+    // int a[5] = {1, 2, 3, 4, 5};
+    // cout << "Element in a[] at index " << 0 << ": " << a[0] << endl;
+    // cout << "Element in a[] at index " << 1 << ": " << a[1] << endl;
+    // cout << endl;
+
+    // for(int i = 0; i<size(a); i++)
+    // cout << "Element in a[] at index " << i << ": " << a[i] << endl;
+
+    // taking input and print values for an array
+    // int arr[5];
+    // for(int i = 0; i<size(arr); i++)
+    // cout << "Element in arr[] at index " << i << ": " << arr[i] << endl;
+    
+    // cout << endl;
+
+    // for(int i = 0; i<size(arr); i++)
+    // {
+    //     cout << "Enter the value for index " << i <<": ";
+    //     cin >> arr[i];
+    // }
+
+    // cout << endl;
+    
+    // for(int i = 0; i<size(arr); i++)
+    // cout << "Element in arr[] at index " << i << ": " << arr[i] << endl;
+
+    return 0;
+}
