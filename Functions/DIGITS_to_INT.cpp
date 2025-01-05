605d09baa092333b16153b5cd0c9e8921734b662
Arihant Bhandari <arihant.bhandari.2004@gmail.com>
2025-01-05 21:49:56 +0530

    Program: Convert DIGITS into INTEGER

---
+++ Functions/DIGITS_to_INT.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int number(int digits[], int size)
+{
+    int num = 0;
+    for(int i = size; i>0; i--)
+    {
+        num = digits[size-i]*pow(10,i-1) + num;
+    }
+
+    return num;
+}
+int main()
+{
+    int n;
+    cout << "Enter no. of digits you want: ";
+    cin >> n;
+
+    if(to_string(n).length() < to_string(INT_MAX).length())
+    {
+        int digits[n];
+
+        int place = pow(10,n-1);
+        for(int i = 0; i<n; i++)
+        {
+            cout << "Enter digit for " << place <<"s place: ";
+            cin >> digits[i];
+            place /= 10; 
+        }
+
+        cout << "Desired integer: " << number(digits, n);
+    }
+    else
+    cout << "INVALID SIZE: expected very large number to be made into INT.";
+
+    return 0;
+}
