003cca724567fc02f4095f367ee50b1bdc44a77d
Arihant Bhandari <arihant.bhandari.2004@gmail.com>
2025-02-12 01:55:26 +0530

    Program: Sorting 0s and 1s in ARRAY

---
+++ Arrays/SORT_0s-1s_in_ARR.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+void count(int arr[], int size)
+{
+    int zero = 0;
+
+    for(int i = 0; i < size; i++)
+    {
+        if(arr[i] == 0)
+        zero++;
+    }
+
+    for(int i = 0; i < size; i++)
+    {
+        if(zero-- > 0)
+        arr[i] = 0;
+        else
+        arr[i] = 1;
+    }
+
+    for(int i = 0; i < size; i++)
+    {
+        cout << arr[i] << endl;
+    }   
+}
+int main()
+{
+    int n;
+    cout << "Enter length of array: ";
+    cin >> n;
+
+    int arr[n];
+
+    for(int i = 0; i<n; i++)
+    {
+        cout << "Enter the value for index " << i <<": ";
+        cin >> arr[i];
+    }
+
+    count(arr, n);
+
+    return 0;
+}
