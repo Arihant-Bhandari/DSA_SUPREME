15ebac43f7964beff6a8caa91577fb3e5b8b60f6
Arihant Bhandari <arihant.bhandari.2004@gmail.com>
2025-02-13 02:19:07 +0530

    Program: Reverse_Diagonal-wise Sum in 2D ARRAY

---
+++ Arrays/REV-DIAG-SUM.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter dimension of array: ";
+    cin >> n;
+
+    int arr[n][n];
+
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            int data;
+            cout << "ENTER ELEMENT FOR CELL [" << i << ", " << j << "]: ";
+            cin >> data;
+            arr[i][j] = data;
+        }
+    }
+    
+    int sum = 0;
+
+    for(int i = 0; i < n; i++)
+    {       
+        sum += arr[i][n-1-i];
+    }
+    
+    cout << "Sum of Diagonal Elements: " << sum << endl;
+    return 0;
+}
