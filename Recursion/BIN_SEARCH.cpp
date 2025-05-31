e05fe5b0a10c9fc385579397a8987971b520d112
Arihant Bhandari <arihant.bhandari.2004@gmail.com>
2025-06-01 01:04:37 +0530

    Program: BINARY SEARCH through RECURSION

---
+++ Recursion/BIN_SEARCH.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int left, int right, int key)
+{
+    int mid = left + (right - left) / 2;
+
+    if(left <= right)
+    {
+        if(arr[mid] == key) return mid;
+        else if(arr[mid] < key) return binarySearch(arr, mid + 1, right, key);
+        else return binarySearch(arr, left, mid - 1, key);
+    }
+
+    return -1;
+}
+int main()
+{
+    int n;
+    cout << "Enter size of Array: ";
+    cin >> n;
+
+    int arr[n];
+
+    for(int i = 0; i < n; i++)
+    {
+        int data;
+        cout << "Enter VALUE at index " << i << ": ";
+        cin >> data;
+
+        arr[i] = data;
+    }
+
+    int key;
+    cout << "Enter key to be searched: ";
+    cin >> key;
+
+    int index = binarySearch(arr, 0, n, key);
+
+    if(index < 0)
+    cout << "KEY not found.";
+    else
+    cout << "KEY found at index: " << index;
+    
+    return 0;
+}
